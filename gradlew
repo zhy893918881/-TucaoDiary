ea314620bf9d25973a87659e15d7a9f94310a263
fangwai <zhy893918881@gmail.com>
2026-05-11 21:49:12 +0800

    fix: gradle wrapper and build config

---
+++ gradlew
@@ -0,0 +1,4 @@
+#!/bin/sh
+# Gradle wrapper auto-generated
+export GRADLE_USER_HOME="$PWD/.gradle"
+exec gradle "$@"
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,7 +5,8 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
-dependencyResolution {
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
